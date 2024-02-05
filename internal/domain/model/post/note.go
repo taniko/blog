af01ae94c0f54004811bfe427aea296aa61f0e9f
taniko <taniko@users.noreply.github.com>
2024-02-05 23:39:09 +0900

    Add functions to create Article, Timeline and Note

--- internal/domain/model/post/note.go
+++ internal/domain/model/post/note.go
@@ -11,3 +11,15 @@ type Note struct {
 	state      State
 	visibility Visibility
 }
+
+func CreateNote(id ID, title Title, body Body, state State, visibility Visibility) Note {
+	return Note{
+		id:         id,
+		title:      title,
+		body:       body,
+		createdAt:  time.Now(),
+		updatedAt:  time.Now(),
+		state:      state,
+		visibility: visibility,
+	}
+}
--- internal/domain/model/post/timeline.go
+++ internal/domain/model/post/timeline.go
@@ -7,7 +7,6 @@ type TimelineID string
 type Timeline struct {
 	id         TimelineID
 	authorID   AuthorID
-	stacks     []Stack
 	visibility Visibility
 	state      State
 	createdAt  time.Time
@@ -24,4 +23,25 @@ type Stack struct {
 	text      string
 	createdAt time.Time
 	updatedAt time.Time
+}
+
+func CreateTimeline(id TimelineID, authorID AuthorID, visibility Visibility, state State) Timeline {
+	return Timeline{
+		id:         id,
+		authorID:   authorID,
+		visibility: visibility,
+		state:      state,
+		createdAt:  time.Now(),
+		updatedAt:  time.Now(),
+	}
+}
+
+func (t Timeline) CreateStack(id StackID, text string) Stack {
+	return Stack{
+		id:        id,
+		timeline:  t.id,
+		text:      text,
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+	}
 }
